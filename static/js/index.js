6637321c18d1951fd53aa9ee20a309a78a3b66a9
gastoneguido <gastoneguido@hotmail.com>
2024-05-08 18:30:54 -0300

    cambio grid + css, base para formulario contacto ambos responsive

--- static/js/index.js
+++ static/js/index.js
@@ -77,3 +77,32 @@ function ShowHideMenu() {
       }
     }
   }        
+
+/* AutoScroll con botones*/
+
+var button = document.getElementById('slide');
+    button.onclick = function () {
+        var container = document.getElementById('slider');
+        sideScroll(container,'right',25,200,20);
+    };
+
+var back = document.getElementById('slideBack');
+    back.onclick = function () {
+        var container = document.getElementById('slider');
+        sideScroll(container,'left',25,200,20);
+    };
+
+function sideScroll(element,direction,speed,distance,step){
+    scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
